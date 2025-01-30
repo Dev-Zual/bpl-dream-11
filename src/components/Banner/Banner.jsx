821b341d787f32006186f78db0449c715d665cf7
Zual Rana <freelancerzual@gmail.com>
2025-01-30 22:47:18 +0600

    added banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,32 @@
+import bannerBg from "../../assets/assets/bg-shadow.png";
+import bannerImg from "../../assets/assets/banner-main.png";
+const Banner = () => {
+  return (
+    <div className="container mx-auto mt-5">
+      <div
+        className="hero min-h-[550px] bg-black/90 rounded-md"
+        style={{
+          backgroundImage: `url(${bannerBg})`,
+        }}
+      >
+        {/* <div className="hero-overlay bg-black/50"></div> */}
+        <div className="hero-content text-neutral-content text-center">
+          <div className="">
+            <div className="flex justify-center mb-5">
+              <img src={bannerImg} alt="" />
+            </div>
+            <h1 className="mb-5 text-5xl font-bold">
+              Assemble Your Ultimate Dream 11 Cricket Team
+            </h1>
+            <p className="mb-5 text-2xl">Beyond Boundaries Beyond Limits</p>
+            <button className="btn bg-yellow-400 border-8 py-5 border-[#191919] ring-1 rounded-md font-semibold text-xl ring-yellow-400">
+              Claim Free Credit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
